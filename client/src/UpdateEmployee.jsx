ee2f45dd18934d391d352b7600e6fe77dbd0d6ec
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2024-03-21 22:40:54 -0500

    completed this project by adding the values to the update page in the form as soon as we click on the update button from employee component

--- client/src/UpdateEmployee.jsx
+++ client/src/UpdateEmployee.jsx
@@ -4,137 +4,160 @@ import axios from "axios"
 
 const UpdateEmployee = ({employeeList}) => {
 
-    const navigate = useNavigate()
-    const location = useLocation()
-    const employeeId = location.pathname.split('/')[2]
-    const user = employeeList.filter(employee => employee.id == employeeId)
-    const [filterUser, setFilterUser] = useState(user)
+//     const navigate = useNavigate()
+//     const location = useLocation()
+//     const employeeId = location.pathname.split('/')[2]
+//     const user = employeeList.filter(employee => employee.id == employeeId)
+//     const [filterUser, setFilterUser] = useState(user)
 
     
 
-    console.log(employeeId)
-    console.log(filterUser)
+//     console.log(employeeId)
+//     console.log(filterUser)
 
-    const [employee, setEmployee] = useState({
-        fName:'',
-        lName:'',
-        email:''
-    })
+//     const [employee, setEmployee] = useState({
+//         fName:'',
+//         lName:'',
+//         email:''
+//     })
 
-const handleFormChange = (e) => {
-    e.preventDefault()
-    const {name, value} = e.target
-    setEmployee(prevState => {
-        return {
-        ...prevState, [name]: value 
-    }})
-}
+// const handleFormChange = (e) => {
+//     e.preventDefault()
+//     const {name, value} = e.target
+//     setEmployee(prevState => {
+//         return {
+//         ...prevState, [name]: value 
+//     }})
+// }
 
-const handleFormSubmit = async(e) => {
-    e.preventDefault()
-    try{
-        await axios.put(`http://localhost:3000/create/`+ employeeId, employee)
+// const handleFormSubmit = async(e) => {
+//     e.preventDefault()
+//     try{
+//         await axios.put(`http://localhost:3000/create/`+ employeeId, employee)
         
-        navigate('/')
-        window.location.reload()
-    } catch(err) {
-        console.log(err)
+//         navigate('/')
+//         window.location.reload()
+//     } catch(err) {
+//         console.log(err)
+//     }
+// }
+
+//     return (
+//         <div>
+//             <Link to='/' className="btn">Back</Link>
+//             <form onSubmit={handleFormSubmit}>
+//                 <h3>Update Employee</h3>
+//                 <input 
+//                     type="text"
+//                     placeholder="First Name"
+//                     name="fName"
+//                     value={employee.fName}
+//                     onChange={handleFormChange}
+//                 />
+//                 <input 
+//                 type="text"
+//                     placeholder="Last Name"
+//                     name="lName"
+//                     value={employee.lName}
+//                     onChange={handleFormChange}
+//                 />
+//                 <input 
+//                 type="email"
+//                     placeholder="Email"
+//                     name="email"
+//                     value={employee.email}
+//                     onChange={handleFormChange}
+//                 />
+//                 <button className="btn">Submit</button>
+//             </form>
+//         </div>
+//     )
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const employeeId = location.pathname.split('/')[2]
+    // const user = employeeList.filter(employee => employee.id == employeeId)
+    // const newUserObject = {
+    //     fName: user[0].fName,
+    //     lName: user[0].lName,
+    //     email: user[0].email
+    // }
+    const [filterUser, setFilterUser] = useState([])
+    
+
+    useEffect(() => {
+        const filterEmployeesList = async() => {
+            const user = await employeeList.filter(employee => employee.id == employeeId)
+            const newUserObject = {
+                fName: user[0].fName,
+                lName: user[0].lName,
+                email: user[0].email
+            }
+            setFilterUser(newUserObject)
+        }
+        filterEmployeesList()
+    }, [employeeId])
+
+    // console.log('user', filterUser)
+    
+
+    
+    const handleFormChange = (e) => {
+        e.preventDefault()
+        const {name, value} = e.target
+        setFilterUser(prevState => (
+             {
+                ...prevState, [name]: value
+            }
+        ))
+        console.log(`filterUser:`,filterUser)
     }
-}
+    
+    const handleFormSubmit = async(e) => {
+        e.preventDefault()
+        try{
+            await axios.put(`http://localhost:3000/create/`+ parseInt(employeeId), filterUser)
+            navigate('/')
+        }
+        catch(err){
+            console.log(err)
+        }
+        console.log(`form submitted`)
+        
+    }
 
     return (
         <div>
             <Link to='/' className="btn">Back</Link>
-            <form onSubmit={handleFormSubmit}>
+            <div>
                 <h3>Update Employee</h3>
-                <input 
+                <input
                     type="text"
                     placeholder="First Name"
                     name="fName"
-                    value={employee.fName}
+                    value={filterUser.fName}
                     onChange={handleFormChange}
                 />
                 <input 
-                type="text"
+                    type="text"
                     placeholder="Last Name"
                     name="lName"
-                    value={employee.lName}
+                    value={filterUser.lName}
                     onChange={handleFormChange}
                 />
                 <input 
-                type="email"
+                    type="email"
                     placeholder="Email"
                     name="email"
-                    value={employee.email}
+                    value={filterUser.email}
                     onChange={handleFormChange}
                 />
-                <button className="btn">Submit</button>
-            </form>
+                <button  onClick={handleFormSubmit} className="btn">Submit</button>
+            </div>
+        
         </div>
     )
-
-    // const { id } = useParams()
-    // const user = employeeList.filter(employee => employee.id == id)
-    // const [filterUser, setFilterUser] = useState(user)
-    // const navigate = useNavigate()
-
-    // console.log('user', filterUser)
-    // console.log('id:', typeof(id))
-
-    
-    // const handleFormChange = (e) => {
-    //     e.preventDefault()
-    //     const {name, value} = e.target
-    //     setFilterUser(prevState => {
-    //         return [{
-    //         ...prevState, [name]: value 
-    //     }]})
-    // }
-    
-    // const handleFormSubmit = async(e) => {
-    //     e.preventDefault()
-    //     try{
-    //         await axios.put(`http://localhost:3000/create/`+ parseInt(id), filterUser)
-    //         navigate('/')
-    //     }
-    //     catch(err){
-    //         console.log(err)
-    //     }
-    //     console.log(`form submitted`)
-        
-    // }
-
-    // return (
-    //     <div>
-    //         <Link to='/' className="btn">Back</Link>
-    //         <div>
-    //             <h3>Update Employee</h3>
-    //             <input
-    //                 type="text"
-    //                 placeholder="First Name"
-    //                 name="fName"
-    //                 value={filterUser[0].fName}
-    //                 onChange={handleFormChange}
-    //             />
-    //             <input 
-    //                 type="text"
-    //                 placeholder="Last Name"
-    //                 name="lName"
-    //                 value={filterUser[0].lName}
-    //                 onChange={handleFormChange}
-    //             />
-    //             <input 
-    //                 type="email"
-    //                 placeholder="Email"
-    //                 name="email"
-    //                 value={filterUser[0].email}
-    //                 onChange={handleFormChange}
-    //             />
-    //             <button  onClick={handleFormSubmit} className="btn">Submit</button>
-    //         </div>
-        
-    //     </div>
-    // )
 }
 
 export default UpdateEmployee
